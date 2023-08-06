eeda2f928b7734b31b2fcea470139ee89586d54a
CareTiger <currency@gmail.com>
2023-08-06 04:54:18 -0500

    Initial commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<template>
+  <LegoSocial>
+    <LegoSocialLink />
+    <LegoSocialFacebook />
+    <LegoSocialTwitter text="Hello world!" />
+    <LegoSocialLinkedIn text="Hello world!" />
+    <LegoSocialReddit text="Hello world!" />
+  </LegoSocial>
+</template>
